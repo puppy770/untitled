55349865b3a19524f3d38c0221f3f49b458c5c7e
yyx <2997336431@qq.com>
2025-01-15 17:40:26 +0800

    hot-fix第二次提交

--- untitled2/src/main/java/com/yang/Test111.java
+++ untitled2/src/main/java/com/yang/Test111.java
@@ -2,7 +2,9 @@ package com.yang;
 
 public class Test111 {
     public static void main(String[] args) {
+        System.out.println("Hello World by master");
         System.out.println("Hello World");
         System.out.println("hello by hot-fix");
+        System.out.println("hello by hot-fix!");
     }
 }
